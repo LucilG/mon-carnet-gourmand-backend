8901c32d9487214d2961fadb4e1b02a8bcdbaea9
Lucille Gris <lucille.gris@gmail.com>
2025-01-23 12:22:52 +0100

    add create method in recipes controller. Tests ok with postman and with frontend app

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -14,4 +14,20 @@ class Api::RecipesController < ApplicationController
 
     render json: @recipe
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save!
+      # TO DO : vérifier l'option location:
+      render json: @recipe, status: :created, location: api_recipes_url(@recipe)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :nb_people, :prepa_time, :cooking_time, :instructions, :collection_id)
+  end
 end
